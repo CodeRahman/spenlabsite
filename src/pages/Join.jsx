c499f502e9043b23ab0eff15736282e3b0d1ad76
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2025-03-10 13:37:45 -0500

    new

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../routes/Layout";
 import Footer from "../components/Footer";
+import './Join.css';
 
 function Join() {
   return (
@@ -8,6 +9,13 @@ function Join() {
         <div className='main'>
         <Layout />
       <h1>Join</h1>
+      <div className="join-message">
+        <p>We are looking for highly motivated and passionate graduate and undergraduate students looking to engage in research in 
+          Security and Privacy of Emerging Networks. Recommended majors include, Computer Science, Information Technology, Cybersecurity, 
+          Software Engineering and other related majors
+        </p>
+      </div>
+      
       </div>
       <Footer />
     </div>
