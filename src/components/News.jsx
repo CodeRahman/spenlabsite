9d5b782b86c6e10bcc5348ba1e2ff0c86cbf3c08
Abdurrahman Oyediran <oyediranabdurrahman@gmail.com>
2025-03-10 13:56:19 -0500

    newsflash

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,6 +6,24 @@ function News() {
         <div className="news-section">
         <h1>News</h1>
         <p>Here are the latest news</p>
+        <ul>
+        <li>Sherif, A. (PI), (2023) "Accepted paper "Privacy-Aware and Hardware Acceleration-Based 
+            Aggregation Scheme for Smart Grid Networks" in the IEEE Eighth International Conference On 
+            Mobile And Secure Services (MobiSecServ) conference," Sponsored by A travel award through the 
+            College of Arts and Sciences, University of Southern Mississippi.</li>
+
+        <li>Sherif, A. (PI), (2023) "Accepted paper "Privacy-Aware and Hardware Acceleration-Based 
+            Aggregation Scheme for Smart Grid Networks" in the IEEE Eighth International Conference On 
+            Mobile And Secure Services (MobiSecServ) conference," Sponsored by A travel award through the 
+            College of Arts and Sciences, University of Southern Mississippi.</li>
+
+        <li>Sherif, A. (PI), (2023) "Accepted paper "Privacy-Aware and Hardware Acceleration-Based 
+            Aggregation Scheme for Smart Grid Networks" in the IEEE Eighth International Conference On 
+            Mobile And Secure Services (MobiSecServ) conference," Sponsored by A travel award through the 
+            College of Arts and Sciences, University of Southern Mississippi.</li>
+        </ul>
+
+        
         </div>
     );
     }
